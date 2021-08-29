40282ecec0d9a48f24889e6e5f422e915ac26f43
sharlotte <seeseafood@naver.com>
2021-08-29 22:02:26 +0900

    h

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const client = new Discord.Client({ ws: { intents: intent_list } })
+const client = new Discord.Client()
 const fs = require("fs")
 const prefix = '!';
 
