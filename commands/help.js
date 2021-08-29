ef3fae4a265aae05ac7206248219bfd5e2e92b15
sharlotte <seeseafood@naver.com>
2021-08-29 22:50:03 +0900

    h

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,9 @@ exports.run = (client, message, args) => {
     let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
     let commandStr = ""
     let embed = new Discord.MessageEmbed().setAuthor("Avaliable Commands", helpImg).setColor("#186de6")
-    let cmd = client.commands.get(command);
-    
+
     client.commands.forEach((x) => {
-        commandStr += `• !${x.name} : **${x.desc}**\n`
+        commandStr += `• !${x.name} : **${x.description}**\n`
     })
 
     embed.addField("Commands: ", commandStr)
