a841b10d2fc27ed55028d98561a9a0b9adabc00c
sharlotte <seeseafood@naver.com>
2021-08-30 17:30:04 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
     message.reply(`${client.ws.ping}ms`);
+    message.reply("pong!");
 };
 
 exports.name = 'ping';
