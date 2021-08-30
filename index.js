892918a7e1dbfaa58a0888b872a1d7f530e336fe
zxc1234205 <ltt0718@naver.com>
2021-08-30 13:44:06 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const fs = require("fs")
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
 const prefix = '!';
 
 client.commands = new Discord.Collection() 
