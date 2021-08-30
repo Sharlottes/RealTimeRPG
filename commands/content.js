38b6fef02a3bbb1254f71081088dca9fee670710
sharlotte <seeseafood@naver.com>
2021-08-30 22:36:46 +0900

    update with deploy

--- commands/content.js
+++ commands/content.js
@@ -5,16 +5,16 @@ exports.run = (client, message, args) => {
     let jsonFiles = fs.readdirSync('./json');
     let kvStrs = ""
     jsonFiles.forEach(f => {
-        if(f.split(".")[1] != args[1]) return;
+        if(f.split(".")[0] != args[0]) return;
 
         let jsonData = fs.readFileSync("./json/" + f);
         JSON.parse(jsonData.toString(), (k, v) => {
-            if(k == arg[2]) kvStrs += (k + ": " + v + "\n"); 
+            if(k == arg[1]) kvStrs += (k + ": " + v + "\n"); 
             return v; 
         });
     });
     let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
-    let embed = new Discord.MessageEmbed().setAuthor(args[1] + " - " + args[2], helpImg).setColor("#186de6")
+    let embed = new Discord.MessageEmbed().setAuthor(args[0] + " - " + args[2], helpImg).setColor("#186de6")
 
     embed.addField("Values: ", kvStrs)
     message.channel.send(embed)
