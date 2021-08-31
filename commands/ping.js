1d28a018070705ecb72a7a057b41e51f19032b5b
sharlotte <seeseafood@naver.com>
2021-08-31 22:52:05 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,10 @@
 const tcpp = require('tcp-ping');
 
 exports.run = (client, message, args) => {
-    message.reply(`${client.ws.ping}ms`);
-    message.reply("pong!");
+    message.reply(`from ${client.user.name}, ${client.ws.ping}ms`);
     if(args[0] !== undefined) {
         let started = new Date().getTime();
-        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`${new Date().getTime() - started}ms`));
+        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`from ${arg[0]}, ${new Date().getTime() - started}ms`));
     }
 }
 
