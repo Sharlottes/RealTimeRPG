f51284f860071de86796824d66e036e4ae425908
sharlotte <seeseafood@naver.com>
2021-08-31 22:53:23 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     message.reply(`from ${client.user.name}, ${client.ws.ping}ms`);
     if(args[0] !== undefined) {
         let started = new Date().getTime();
-        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`from ${arg[0]}, ${new Date().getTime() - started}ms`));
+        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`from ${args[0]}, ${new Date().getTime() - started}ms`));
     }
 }
 
