9a5beadb66f1dbcdce01881350add5d792d972a0
sharlotte <seeseafood@naver.com>
2021-08-31 22:49:04 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -3,9 +3,11 @@ const tcpp = require('tcp-ping');
 exports.run = (client, message, args) => {
     message.reply(`${client.ws.ping}ms`);
     message.reply("pong!");
-    if(args[0] !== undefined) 
-        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.channel.send(available));
-};
+    if(args[0] !== undefined) {
+        let started = new Date().getTime();
+        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`${new Date().getTime() - started}ms`));
+    }
+}
 
 exports.name = 'ping';
 exports.description = 'check bot status';
