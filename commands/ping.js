b842e394f38b3b98bd99bbd36d54adcedb7d3acd
sharlotte <seeseafood@naver.com>
2021-08-31 23:04:12 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,19 @@
 const tcpp = require('tcp-ping');
+const fs = require('fs');
+const request = require ('request')
 
-exports.run = (client, message, args) => {
-    message.reply(`from ${client.user.tag}, ${client.ws.ping}ms`);
+
+
+exports.run = (client, message, args) => {request ({
+	url: 'https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_v7.json',
+	json: true
+}, (error, response, body) => !error && response.statusCode === 200? message.channel.send(body): message.channel.send(error))
+    message.reply("pong!");
+    message.channel.send("ps: server is on us");
+    message.channel.send(`${client.user.tag}, ${client.ws.ping}ms`);
     if(args[0] !== undefined) {
         let started = new Date().getTime();
-        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.reply(`from ${args[0]}, ${new Date().getTime() - started}ms`));
+        tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.channel.send(`${args[0]}, ${new Date().getTime() - started}ms`));
     }
 }
 
