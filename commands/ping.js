98943757e5fa5d35a7dab2bdd316dce64cef27b7
sharlotte <seeseafood@naver.com>
2021-08-31 23:32:51 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -6,23 +6,35 @@ let server = [{
 }]
 let serverIndex = 0;
 exports.run = (client, message, args) => {
-    request ({
-	    url: 'https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_v7.json',
-	    json: true
-    }, (error, response, body) => !error && response.statusCode === 200 ? JSON.parse(JSON.stringify(body), (k, v) => {
-        if(k === "name") server[serverIndex].name = v;
-        if(k === "address") server[serverIndex].address = v;
-        if(server[serverIndex].name != "" && server[serverIndex].address != "") {
-            console.log(server);
-            serverIndex++; 
-            server[serverIndex] = {name: "", address: ""};
-        }
-        return v;
-    }) : console.log(error))
     message.reply(`${client.ws.ping}ms`);
     if(args[0] !== undefined) {
         let started = new Date().getTime();
         tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.channel.send(`${args[0]}, ${new Date().getTime() - started}ms`));
+        if(args[0] == "be"){
+            let url = 'https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_v7.json'
+            request ({url: url, json: true}, (error, response, body) => {
+                if(!error && response.statusCode === 200){
+                    JSON.parse(JSON.stringify(body), (k, v) => {
+                        if(k == "name") server[serverIndex].name = v;
+                        if(k == "address") server[serverIndex].address = v;
+                        if(server[serverIndex].name != "" && server[serverIndex].address != "") {
+                            console.log(server);
+                            serverIndex++;
+                            server[serverIndex] = {name: "", address: ""};
+                        }
+                        return v;
+                    });
+                    let embed = new Discord.MessageEmbed().setAuthor(`BE servers`, helpImg).setColor("#186de6");
+                    servers.array.forEach(element => {
+                        let str = "";
+                        element.address.split(", ").forEach(add => str += `• ${add}\n`);
+                        embed.addField(element.name, str);
+                    });
+                    message.channel.send(embed);
+                }
+                else console.log(error);
+            });
+        }
     }
 }
 
