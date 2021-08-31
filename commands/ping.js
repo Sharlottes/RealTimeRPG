324501ffa3a9e0bfca66578fbaaabd717cded7b4
sharlotte <seeseafood@naver.com>
2021-09-01 00:09:52 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -6,15 +6,15 @@ exports.run = (client, message, args) => {
     if(args[0] !== undefined) {
         let started = new Date().getTime();
         tcpp.probe(args[0], args[1] === undefined ? 6567 : args[1], (err, available) => message.channel.send(`${args[0]}, ${new Date().getTime() - started}ms`));
-        if(args[0] == "be"){
+        if(args[0] === "v7" || args[0] === "v6" || args[0] === "be"){
             let server = [{
                 name: "",
                 address: ""
             }]
             let serverIndex = 0;
-            let url = 'https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_v7.json'
+            let url = `https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_${args[0]}.json`
             let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
-            let embed = new Discord.MessageEmbed().setAuthor(`BE servers`, helpImg).setColor("#186de6");
+            let embed = new Discord.MessageEmbed().setAuthor(`${args[0]} servers`, helpImg).setColor("#186de6");
             request({url: url, json: true}, (error, response, body) => {
                 if(!error && response.statusCode === 200){
                     JSON.parse(JSON.stringify(body), (k, v) => {
