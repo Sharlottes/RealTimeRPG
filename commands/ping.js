fa39d557ff60ba6fe41784e6c7301548f265aba4
sharlotte <seeseafood@naver.com>
2021-09-01 16:31:19 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -6,37 +6,27 @@ exports.run = (client, message, args) => {
     message.reply(`${client.ws.ping}ms`);
     if(args[0] !== undefined) {
         if(args[0] === "v7" || args[0] === "v6" || args[0] === "be"){
-            let server = [{
-                name: "",
-                address: ""
-            }]
-            let serverIndex = 0;
             let url = `https://raw.githubusercontent.com/Anuken/Mindustry/master/servers_${args[0]}.json`
             let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
             let embed = new Discord.MessageEmbed().setAuthor(`${args[0]} servers`, helpImg).setColor("#186de6");
             request({url: url, json: true}, (error, response, body) => {
                 if(!error && response.statusCode === 200){
-                    let parsed = JSON.parse(JSON.stringify(body), (k, v) => {
-                        if(k == "name") server[serverIndex].name = v;
-                        if(k == "address") server[serverIndex].address = v;
-                        if(server[serverIndex].name != "" && server[serverIndex].address != "") 
-                            server[++serverIndex] = {name: "", address: ""};
-
-                        
-                        server.forEach((x, indexx, arrs) => {
-                            (x.address+'').split(",").forEach((str, index, arr) => {
-                                let started = new Date().getTime();
-                                tcpp.probe((str+'').split(":")[0], (str+'').split(":")[1] === undefined ? 6567 : (str+'').split(":")[1], (err, available) => {
-                                    if(indexx == arrs.length - 1) embed.addField(x.name, `${str} - ${new Date().getTime() - started}ms\n`);
-                                    if(indexx == arrs.length - 1) message.channel.send(embed);
-                                });
-                            });
-                        });
-                        return v;
-                    });
+                    let parsed = JSON.parse(JSON.stringify(body));
 
                     parsed.forEach((v, i, a) => {
                         let parsedParsed = JSON.parse(JSON.stringify(v));
+                        let name = parsedParsed["name"];
+                        let address = (parsedParsed["address"]+'').replace("[", "").replace("]", "");
+                        let arr = (address+'').split(", ");
+                        let field = ``;
+                        arr.forEach(str => {
+                            let started = new Date().getTime();
+                            tcpp.probe((str+'').split(":")[0], (str+'').split(":")[1] === undefined ? 6567 : (str+'').split(":")[1], (err, available) => {
+                                field += `${str} - ${new Date().getTime() - started}ms\n`;
+                                if(arr.indexOf(str) == arr.length - 1) embed.addField(name, field);
+                            });
+                        });
+                        message.channel.send(embed)
                         console.log(parsedParsed["address"]);
                     });
                 }
