373c0c05562bc9fa61c30cf74fed91bf059612fc
sharlotte <seeseafood@naver.com>
2021-09-01 16:50:48 +0900

    update with deploy

--- commands/ping.js
+++ commands/ping.js
@@ -13,8 +13,7 @@ exports.run = (client, message, args) => {
                     let parsed = JSON.parse(JSON.stringify(body));
                     let embed = new Discord.MessageEmbed().setAuthor(`${args[0]} Servers`, helpImg).setColor("#186de6");
 
-                    parsed.forEach()
-                    parsed.forEach((v, index, array) => {
+                    parsed.forEach(v => {
                         let parsedParsed = JSON.parse(JSON.stringify(v));
                         let name = parsedParsed["name"];
                         let address = (parsedParsed["address"]+'').replace("[", "").replace("]", "");
@@ -25,7 +24,7 @@ exports.run = (client, message, args) => {
                             tcpp.probe((str+'').split(":")[0], (str+'').split(":")[1] === undefined ? 6567 : (str+'').split(":")[1], (err, available) => {
                                 field += `${str} - ${new Date().getTime() - started}ms\n`;
                                 if(arr.indexOf(str) == arr.length - 1) embed.addField(name, field);
-                                if(array.indexOf(v) == arary.length - 1) message.channel.send(embed);
+                                if(parsed.indexOf(v) == arary.length - 1) message.channel.send(embed);
                             });
                         });
                     });
