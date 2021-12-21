ea3c6bdfa12a765d4f0c5b082e74ac6008a361fa
AmteurPotion <1k0j2i4@gmail.com>
2021-12-21 12:58:23 +0900

    인터페이스 & 클래스로 바꾼 겸 그에 맞춰 코드스타일을 바꿔 봄

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,5 +1,20 @@
-namespace test {
+import Discord from "discord.js";
+import firebase from "firebase-admin";
 
+import { Command } from "./index";
+
+class Test implements Command {
+    public readonly name: string;
+    public readonly description: string;
+    
+    constructor(...params: any[]) {
+        this.name = "test";
+        this.description = "test smth";
+    }
+
+    public run (client: Discord.Client, message: Discord.Message, args: string[]) {
+
+    }
 }
 
-export default test;
+export default Test;
