ee882c85509a49b70c1d1df0143413bafdcf7059
AmteurPotion <1k0j2i4@gmail.com>
2021-12-21 13:00:13 +0900

    체인지!

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,8 +3,13 @@ import commands , { Command } from "./index";
 
 class Help implements Command {
     // 타입을 제대로 지정하면 자동완성의 도움이 지리니까 할 수 있다면 해놓는 걸 추천.
-    public readonly name = "help";
-    public readonly description = "show avaliable commands";
+    public readonly name: string;
+    public readonly description: string;
+
+    constructor(...params: any[]) {
+        this.name = "help";
+        this.description = "show avaliable commands";
+    }
     
     public run (client: Client, message: Message, args: string[]) {
         let helpImg = "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png"
