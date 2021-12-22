ba88281856cbd0dbe3cf1a6ffe24e270f9273227
AmteurPotion <1k0j2i4@gmail.com>
2021-12-22 17:59:25 +0900

    파일명 소문자 -> 대문자 바꾸기 작업

---
+++ src/commands/Test.ts
@@ -0,0 +1,43 @@
+import Discord from "discord.js";
+import firebase from "firebase-admin";
+
+import { Command } from "./index";
+
+class Test implements Command {
+    public readonly name: string;
+    public readonly description: string;
+    public readonly app: firebase.app.App;
+    public readonly database: firebase.database.Database;
+
+    private firebaseConfig: firebase.AppOptions;
+
+    constructor(...params: any[]) {
+        this.name = "test";
+        this.description = "test smth";
+
+        this.firebaseConfig = {}
+        this.app = firebase.initializeApp(this.firebaseConfig);
+        this.database = firebase.database();
+    }
+
+    public run (client: Discord.Client, message: Discord.Message, args: string[]): void {
+        const database = this.database;
+        
+        try {
+            firebase.database().goOnline();
+    
+            database.ref("/Archive/" + message.author.id).set({
+                Message: message.content,
+                User: message.author.username,
+                User_ID: message.author.id,
+            });
+            firebase.database().goOffline();
+    
+            message.channel.send(`hi!`);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+}
+
+export default Test;
