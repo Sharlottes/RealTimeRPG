f17eecf0a69a5606381e9dec7c615625591110aa
AmteurPotion <1k0j2i4@gmail.com>
2021-12-23 15:09:07 +0900

    content 포팅 끗

--- src/commands/Content.ts
+++ src/commands/Content.ts
@@ -17,11 +17,11 @@ class Content implements Command {
     public run(client: Client, message: Message, args: string[]): void {
         console.log("content commands is called with" + JSON.stringify(args));
 
+        const jsonFiles: string[] = fs.readdirSync("./json");
+        const kvStrs: string[] = [""];
+        const files: string[] = [""];
         let stop: boolean = false;
-        let jsonFiles: string[] = fs.readdirSync("./json");
-        let kvStrs: string[] = [""];
         let keys: string[] = [""];
-        let files: string[] = [""];
         let fileIndex: number = 0,
             keyIndex: number = 0,
             kvIndex = 0;
@@ -30,12 +30,12 @@ class Content implements Command {
             if(f !== undefined) { files[fileIndex++] = f; }
             if(stop || args[0] === undefined || args[0] != f.split(".")[0]) return;
 
-            let jsonBuffer: Buffer = fs.readFileSync("./json/" + f);
-            let jsonData: any = JSON.parse(jsonBuffer.toString());
+            const jsonBuffer: Buffer = fs.readFileSync("./json/" + f);
+            const jsonData: any = JSON.parse(jsonBuffer.toString());
             keys = Object.keys(jsonData);
 
             if(jsonData[args[1]] !== undefined) {
-                let jsonData2: any = JSON.parse(JSON.stringify(jsonData[1]));
+                const jsonData2: any = JSON.parse(JSON.stringify(jsonData[1]));
                 Object.keys(jsonData2).forEach(k => {
                     if(args[2] !== undefined) {
                         if(k.includes(args[2])) {
@@ -49,9 +49,67 @@ class Content implements Command {
             }
         });
 
-        let helpImg: string =
+        const helpImg: string =
           "https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png";
+        const maxLength: number = 1000;
         let str: string = "";
+
+        if(!stop || args[0] === undefined) {
+            message.channel.send("**content type is not found!");
+
+            files.forEach((k) => {
+                str += `• !content **${k.replace(".json", "")}** <content> <value>\n`;
+                if(str.length >= maxLength || files.indexOf(k) == files.length - 2) {
+                    message.channel.send(
+                        new Discord.MessageEmbed()
+                            .setAuthor("All Content Types", helpImg)
+                            .setColor("#186de6")
+                            .addField("Commands", str)
+                    )
+                    str = "";
+                }
+            });
+        } else if(args[1] === undefined) {
+            message.channel.send(`**${args[0]} content is not found!**`);
+
+            keys.forEach((k) => {
+                if(k !== "immunities" && k !== "") {
+                    str += `• !content ${args[0]} **${k}** <value>\n`;
+                }
+            })
+
+            for (let i = 0; i < str.length % maxLength; i++) {
+                const sstr: string = str.slice(
+                    i * maxLength,
+                    Math.min(str.length, (i + 1) * maxLength)
+                );
+
+                if(sstr !== "") {
+                    message.channel.send(
+                        new Discord.MessageEmbed()
+                            .setAuthor(`All ${args[0]} contents`, helpImg)
+                            .setColor("#186de6")
+                            .addField(`Commands`, sstr)
+                    );
+                }
+            }
+        } else {
+            kvStrs.forEach(kv => {
+                str += `${kv}\n`;
+
+                if(str.length >= maxLength || kvStrs.indexOf(kv) == kvStrs.length - 1) {
+                    message.channel.send(
+                        new Discord.MessageEmbed()
+                            .setAuthor(`All ${args[0]} - ${args[1]} Values`, helpImg)
+                            .setColor("#186de6")
+                            .addField(`Values`, str)
+                    );
+
+                    str = "";
+                }
+            })
+        }
+        message.reply("done");
     }
 
 }
