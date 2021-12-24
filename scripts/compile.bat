946f0135c338fe9614a414cc26d7600a38044d9e
AmteurPotion <1k0j2i4@gmail.com>
2021-12-24 16:05:47 +0900

    1. 가독성을 위해 빌드 스크립트 분리. 2. 애셋 파일들 컴파일 방법 변경 3. 애셋 불러오는 코드 구현 준비

---
+++ scripts/compile.bat
@@ -0,0 +1,9 @@
+@echo on
+@echo 이전 컴파일 데이터 삭제중...
+@start /b tsc --build --clean
+@if exist build\compile (rd /s /q build\compile)
+@echo 컴파일 중...
+@start /b tsc --build
+@echo off
+@mkdir %cd%\build\compile\assets
+@xcopy %cd%\assets %cd%\build\compile\assets /s /e /h /k /y
