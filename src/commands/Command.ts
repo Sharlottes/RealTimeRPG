76139c4acc0c9b3ee489f66e0ae13ac40be68ac5
AmteurPotion <1k0j2i4@gmail.com>
2021-12-29 17:35:43 +0900

    readonly

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -3,8 +3,8 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 
 // 타입스크립트에선 인터페이스보단 타입 형식이 좀 더 지원폭이 넒기에 그냥 type으로 선언함.
 type Command = {
-  builder: SlashCommandBuilder;
-  run: (interaction: CommandInteraction) => void;
+  readonly builder: SlashCommandBuilder;
+  readonly run: (interaction: CommandInteraction) => void;
 };
 
 export default Command;
