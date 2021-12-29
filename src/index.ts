5101c0a464232d27384906a1f5b34380fddd3ca2
AmteurPotion <1k0j2i4@gmail.com>
2021-12-29 17:16:16 +0900

    message 방식 명령어 시스템에서 interaction(상호 작용 방식)으로 변경

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,21 @@ export { default as firebaseAdmin } from "./FirebaseAdmin"
 // 이렇게 하면 불러온 모듈들이 모두 하나의 변수를 통해 참조?가 되지만 다른 방식으로 할 수도 있음.
 // import { Client } from "discord.js";
 // 위 같은 방식으로 불러오면 해당 패키지내의 모든 모듈을 불러오지 않고 해당 모듈 하나만 불러옴.
-import Discord, { Client, Message } from "discord.js";
+import Discord, { Client, Intents } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Routes } from "discord-api-types/v9";
 
 // 직접 쓴 코드도 같은 방식으로 불러올 수 있음.
 import { firebaseAdmin } from ".";
-import commands from "./commands";
+import commands, { Command } from "./commands";
 import assets from "./assets"
 import config from "./config.json";
 
 // 파이어베이스 초기화
 
 const admin = firebaseAdmin;
+const startArgs: Map<string, any> = new Map<string, any>();
 
-const args: Map<string, any> = new Map<string, any>();
 // 기본 인자 설정
 
 // 프로그램 실행 인자 추출
@@ -26,25 +28,68 @@ process.argv.forEach((arg: string, index: number, array: string[]) => {
     const value: string = array[index + 1];
     
     if(value == "true" || value == "false") {
-      args.set(key, Boolean(value));
+      startArgs.set(key, Boolean(value));
     } else if(Number(value).toString() != "NaN") {
-      args.set(key, Number(value));
+      startArgs.set(key, Number(value));
     } else {
-      args.set(key, value);
+      startArgs.set(key, value);
     }
   }
 });
 
 assets.init(config.debug);
-
 // Discord.Client에 commands 속성이 없길래 그냥 다른 객체로 분리함.
-const client: Client = new Discord.Client(); 
-const prefix = "!";
+const client: Client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); 
 
+function toSlashCommand(command: Command): SlashCommandBuilder {
+  const result = new SlashCommandBuilder()
+  .setName(command.name)
+  .setDescription(command.description || "no description");
+// 명령어 인수에 이름좀 붙여줘요.. ex) arg[0] 이럴경우 => type: string / id: name / require(필수 입력인 경우): true
+// 이렇게 해당 인수 위에 주석 붙여주심 댐
+// 그걸 지금 option으로 추가? 변경해야되갖구
+  command.build(result);
+
+  return result;
+}
 // 속성 뒤에 ?는 해당 값이 널인지 확인하고 널이 아니면 실행
-// ps. !가 아니라 ? 인데 지금 확인하고 고침..
-client.on("ready", () => console.log(`Logged in as ${client.user?.tag}!`));
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`)
 
+  // 봇 시작시 로딩
+  commands.forEach(command => {
+    client.application?.commands.create(toSlashCommand(command).toJSON());
+  })
+});
+
+// 명령어 구현부
+client.on("interactionCreate", async interaction => {
+  if(!interaction.isCommand()) return;
+
+  if(commands.has(interaction.commandName)) {
+    const whiteList: any = config.whiteList;
+    let channelName: string =
+      interaction.channel instanceof Discord.TextChannel ||
+      interaction.channel instanceof Discord.NewsChannel
+        ? interaction.channel.name
+        : interaction.user.username;
+
+    if((whiteList == false || whiteList.includes(channelName)) || interaction.channel?.type == "DM") {
+      commands.get(interaction.commandName)?.run(interaction);
+    } else {
+      interaction.reply("Commands are available only in the dm channel or the following channels.")
+    }
+  } else {
+    interaction.reply("Error... Undefined command!");
+  }
+  
+  if(!interaction.replied) {
+    interaction.reply("no reply");
+  }
+});
+
+/* 전 버전 방식 명령어 구현부
+const prefix = "!";
 client.on("message", (message: Message) => {
   if (message.author.bot) return; //not botself
   if (!message.content.startsWith(prefix)) return; //need command tag
@@ -58,18 +103,17 @@ client.on("message", (message: Message) => {
     let name: string = message.channel instanceof Discord.TextChannel || message.channel instanceof Discord.NewsChannel ?
     message.channel.name : message.author.username;
 
-    if((whiteList == false || whiteList.includes(name)) || message.channel.type == "dm") {
-      commands.get(command)?.run(client, message, args);
+    if((whiteList == false || whiteList.includes(name)) || message.channel.type == "DM") {
     }
   }
 });
+*/
 
-//client.login(process.env.token);
 client.login(config.botToken);
 
 const app = {
   client: client,
-  option: args,
+  option: startArgs,
   config: config
 }
 
