2fb6d8a9b2caab895151a7d78f8288a79c22a530
AmteurPotion <1k0j2i4@gmail.com>
2021-12-29 17:16:53 +0900

    run 하나로 모든 걸 처리하는 것에서 build & run 방식으로 바꿈

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,10 +1,12 @@
-import { Client, Message } from "discord.js";
+import Discord, { CommandInteraction } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 // 타입스크립트에선 인터페이스보단 타입 형식이 좀 더 지원폭이 넒기에 그냥 type으로 선언함.
 type Command = {
   name: string;
   description: string;
-  run: (client: Client, message: Message, args: string[]) => void;
+  build: (builder: SlashCommandBuilder) => void;
+  run: (interaction: Discord.CommandInteraction) => void;
 };
 
 export default Command;
