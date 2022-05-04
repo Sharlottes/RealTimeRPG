7b39f370435d4c0d2af3dd997f1960f4f21d6db0
sharlottes <seeseafood@naver.com>
2022-05-05 01:02:28 +0900

    code clean up

--- src/index.ts
+++ src/index.ts
@@ -6,60 +6,27 @@ import CM from "@뇌절봇/commands";
 import assets from "@뇌절봇/assets"
 import config from "@뇌절봇/config.json"
 
-//RTTRPG
 import { init } from './game/rpg_';
-import Kakao from "./kakao/Kakao";
 import { onDiscordMessage } from "./kakao";
 
-/*
-Array.prototype.splitIndex = function(index: number) {
-  return [this.slice(0, index), this.slice(index, this.length-1)];
-}
-*/
-
-const masterIDs: string[] = [
+const masterIDs = [
     "462167403237867520",
     "473072758629203980"
 ];
-// 파이어베이스 초기화
-firebaseAdmin;
 
-// discord rest api 호출
-const rest = new REST({ version: '9' }).setToken(config.botToken);
-
-// 프로그램 초기화
 // App 선언 - 봇의 모든 코드를 관리함
 const app = {
     client: new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }),
     option: new Map<string, boolean|number|string>(),
     config: config,
-    rest: rest
+    rest: new REST({ version: '9' }).setToken(config.botToken)
 };
 
 const { client, option } = app;
 
-// 프로그램 실행 인자 추출
-process.argv.forEach((arg: string, index: number, array: string[]) => {
-    if(arg.slice(0, 2) == "--" && array.length > index + 1) {
-        const key: string = arg.slice(2);
-        const value: string = array[index + 1];
-    
-        if(value == "true" || value == "false") {
-            option.set(key, Boolean(value));
-        } else if(Number(value).toString() != "NaN") {
-            option.set(key, Number(value));
-        } else {
-            option.set(key, value);
-        }
-  }
-});
-
-// 애셋 파일 로딩
-assets.init(config.debug);
-
 // 길드 초기화
 function guildInit(guild: Discord.Guild) {
-    firebaseAdmin.firestore.collection(guild.id).doc("config").get().then(snapshot => {
+    return firebaseAdmin.firestore.collection(guild.id).doc("config").get().then(snapshot => {
         if(!snapshot.exists) {
             const root = firebaseAdmin.firestore.collection(guild.id);
             root.doc("config").set({
@@ -71,15 +38,33 @@ function guildInit(guild: Discord.Guild) {
     });
 }
 
+// 프로그램 실행 인자 추출
+for(let i = 0; i < process.argv.length; i += 2) {
+    const arg = process.argv[i];
+    const value = (()=>{
+        const value = process.argv[i + 1];
+        if(value == 'true') return true;
+        if(value == 'false') return false;
+        if(isNaN(Number(value))) return Number(value);
+        return value;
+    })();
+    
+    if(arg.startsWith('--') && value) option.set(arg.slice(2), value);
+}
+
+// 애셋 파일 로딩
+assets.init(config.debug);
+console.log('asset initialization has been done')
+
 client.once("ready", () => {
     console.log(`Logged in as ${client.user?.tag}(${client.application?.id})!`)
     
     // 봇 명령어 초기화
-    CM.refreshCommand("global");
+    CM.refreshCommand("global").then(()=>console.log('global commend refreshing has done'));
     
     // 서버마다 데이터베이스 체크
     client.guilds.cache.forEach(guild => {
-        guildInit(guild);
+        guildInit(guild).then(()=>console.log(`${guild.name} database checking has done`));
     })
 });
 
@@ -90,72 +75,39 @@ client.on("guildCreate", guild => {
 // 명령어 구현부
 client.on("interactionCreate", async interaction => {
     if(interaction.isCommand()) {
+        const command = CM.commands.get(interaction.commandName);
+        if(!command || !interaction.channel) return;
         await interaction.deferReply();
-        try {
-            if(CM.commands.has(interaction.commandName)) {
-                const whiteList = config.whiteList as false | string[];
-                const command = CM.commands.get(interaction.commandName);
-
-                const channelName: string =
-                    interaction.channel instanceof Discord.TextChannel ||
-                    interaction.channel instanceof Discord.NewsChannel
-                        ? interaction.channel.name
-                        : interaction.user.username;
-            
-                if((whiteList == false || whiteList.includes(channelName)) && (interaction.channel?.type == "DM" || !command?.dmOnly)) {
-                    command?.run(interaction);
-                } else {
-                    interaction.editReply({
-                        content: "This command is available only in the dm channel or the following channels."
-                    })
-                }
-            } else {
-                interaction.editReply({
-                    content: "Error... Undefined command!"
-                });
+    
+        if(interaction.channel.type == "DM" || !command.dmOnly) {
+            try {
+                command.run(interaction);
+            } catch(e) {
+                interaction.editReply((e as object).toString());
+                console.log(e);
             }
-        } catch(error) {
-            interaction.editReply({content: "error: "+error});
-            console.log(error);
         }
+        else interaction.editReply("This command is available only in the dm channel.");
     }
-
-    return;
 });
 
 client.on("messageCreate", async message => {
     if(message.author.id != client.user?.id) onDiscordMessage(message);
 
     if(message.channel.type != "DM" && message.content == "!refresh" && message.guild != null && 
-        (message.author.id == message.guild?.ownerId || masterIDs.includes(message.author.id))) {
-        try {
-            const time = new Date().getTime();
-            const guild = message.guild;
-            message.reply("refresh start! server: " + guild.name);
-            
-            (()=>{
-                const p = CM.refreshCommand("guild", guild);
-                init();
-                return p;
-            })().then(() => {
-                message.reply(`refresh finished in ${(Date.now() - time) / 1000}s`);
-            }).catch(e => {
-                message.reply(e+"");
-            }); 
-        } catch (error) { 
-            message.reply({content: "error: "+error});
-        }
+        (message.author.id == message.guild.ownerId || masterIDs.includes(message.author.id))) {
+        const time = new Date().getTime();
+        const guild = message.guild;
+        message.reply("refresh start! server: " + guild.name);
+        
+        CM.refreshCommand("guild", guild).then(init)
+        .then(() => message.reply(`refresh finished in ${(Date.now() - time) / 1000}s`))
+        .catch(e => message.reply(e+"")); 
     }
 });
 
-//Kakao.init();
+CM.reloadCommands().then(init).then(()=>client.login(config.botToken));
 
-(()=>{
-    const pros = CM.reloadCommands();
-    init();
-    return pros;
-})().then(() => {
-    client.login(config.botToken);
-});
+//Kakao.init();
 
 export default app;
